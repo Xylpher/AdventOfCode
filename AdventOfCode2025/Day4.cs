4874cedd31ee4a3618941dae119ab2d161fe1f97
Xylpher <140599344+Xylpher@users.noreply.github.com>
2025-12-04 15:53:40 +0100

    Day 4 - 2025

---
+++ AdventOfCode2025/Day4.cs
@@ -0,0 +1,92 @@
+﻿using AdventOfCode.Utils;
+
+namespace AdventOfCode2025
+{
+    internal class Day4 : Day
+    {
+        Dictionary<int, int, char> values = new Dictionary<int, int, char>();
+
+
+        public Day4() : base(4)
+        {
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                var line = lines.ElementAt(i);
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == '@')
+                        values.Add(i, j, line[j]);
+                }
+            }
+        }
+
+        public override string ProblemOne()
+        {
+            int result = 0;
+            foreach (var key in values.Keys)
+            {
+                if (CheckPosDic(key.Item1, key.Item2))
+                    result++;
+            }
+            return result.ToString();
+        }
+
+
+        private bool CheckPosDic(int x, int y)
+        {
+            var adjacentRolls = 0;
+            if (values.ContainsKey(x - 1, y - 1))
+                adjacentRolls++;
+            if (values.ContainsKey(x - 1, y))
+                adjacentRolls++;
+            if (values.ContainsKey(x - 1, y + 1))
+                adjacentRolls++;
+            if (values.ContainsKey(x, y - 1))
+                adjacentRolls++;
+            if (values.ContainsKey(x, y + 1))
+                adjacentRolls++;
+            if (values.ContainsKey(x + 1, y - 1))
+                adjacentRolls++;
+            if (values.ContainsKey(x + 1, y))
+                adjacentRolls++;
+            if (values.ContainsKey(x + 1, y + 1))
+                adjacentRolls++;
+
+            return adjacentRolls < 4;
+        }
+
+        public override string ProblemTwo()
+        {
+            int result = 0;
+            List<Tuple<int, int>> changeValues = new List<Tuple<int, int>>();
+            bool changeDone = true;
+            while (changeDone)
+            {
+                foreach (var key in values.Keys)
+                {
+                    if (CheckPosDic(key.Item1, key.Item2))
+                    {
+                        result++;
+                        changeValues.Add(key);
+                    }
+                }
+                if (changeValues.Count > 0)
+                {
+                    foreach (var key in changeValues)
+                    {
+                        values.Remove(key);
+                    }
+                    changeValues.Clear();
+                }
+                else
+                {
+                    changeDone = false;
+                }
+            }
+
+            return result.ToString();
+        }
+
+    }
+}
+
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,7 +1,7 @@
 ﻿using AdventOfCode.Utils;
 using AdventOfCode2025;
 
-Day day = new Day3();
+Day day = new Day4();
 
 Console.WriteLine(day.ProblemOne());
 Console.WriteLine(day.ProblemTwo());
