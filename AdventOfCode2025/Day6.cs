a206ce4030c87ce7ed27ad8714bf4360a4fce6a6
Xylpher <140599344+Xylpher@users.noreply.github.com>
2025-12-06 10:18:55 +0100

    Day 6 -2025

---
+++ AdventOfCode2025/Day6.cs
@@ -0,0 +1,91 @@
+﻿using AdventOfCode.Utils;
+
+namespace AdventOfCode2025
+{
+    internal class Day6 : Day
+    {
+        public Day6() : base(6)
+        {
+        }
+
+        public override string ProblemOne()
+        {
+            List<IEnumerable<string>> splits = new List<IEnumerable<string>>();
+            foreach (var line in lines)
+            {
+                var split = line.Split(' ').ToList();
+                split.RemoveAll(x => String.IsNullOrWhiteSpace(x));
+                splits.Add(split);
+            }
+
+            long result = 0;
+            for (int i = 0; i < splits.First().Count(); i++)
+            {
+                var op = splits.Last().ElementAt(i);
+
+                long mathResult = Convert.ToInt64(splits.First().ElementAt(i));
+                for (int j = 1; j < splits.Count() - 1; j++)
+                {
+                    switch (op)
+                    {
+                        case "*":
+                            mathResult *= Convert.ToInt32(splits.ElementAt(j).ElementAt(i));
+                            break;
+                        case "+":
+                            mathResult += Convert.ToInt64(splits.ElementAt(j).ElementAt(i));
+                            break;
+                    }
+                }
+                result += mathResult;
+            }
+            return result.ToString();
+        }
+        public override string ProblemTwo()
+        {
+            long result = 0;
+
+            var lineLength = lines.First().Length;
+            var op = ' ';
+            List<string> numbers = new List<string>();
+
+            for (int i = lineLength - 1; i >= 0; i--)
+            {
+                string nextNumber = "";
+                for (int j = 0; j < lines.Count() - 1; j++)
+                {
+                    nextNumber += lines.ElementAt(j).ElementAt(i);
+                }
+                numbers.Add(nextNumber);
+                try
+                {
+                    op = lines.Last().ElementAt(i);
+                }
+                catch (Exception ex) { }
+
+                if (op != ' ')
+                {
+                    numbers.RemoveAll(x => String.IsNullOrWhiteSpace(x));
+
+                    long mathResult = Convert.ToInt32(numbers[0]);
+                    for (int z = 1; z < numbers.Count(); z++)
+                    {
+                        switch (op)
+                        {
+                            case '*':
+                                mathResult *= Convert.ToInt32(numbers[z]);
+                                break;
+                            case '+':
+                                mathResult += Convert.ToInt32(numbers[z]);
+                                break;
+                        }
+                    }
+                    numbers.Clear();
+                    op = ' ';
+                    result += mathResult;
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
+
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,7 +1,7 @@
 ﻿using AdventOfCode.Utils;
 using AdventOfCode2025;
 
-Day day = new Day4();
+Day day = new Day6();
 
 Console.WriteLine(day.ProblemOne());
 Console.WriteLine(day.ProblemTwo());
